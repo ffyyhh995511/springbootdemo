796c4a60a3031dd1aa9ef8937b781deb7ec626c2
方云鹤 <fangyunhe@mingbikes.com>
2017-07-17 21:13:04 +0800

    配置

--- src/main/java/com/test/demo/controller/LogBackController.java
+++ src/main/java/com/test/demo/controller/LogBackController.java
@@ -2,19 +2,20 @@ package com.test.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/logBack")
 public class LogBackController {
-	Logger logger = Logger.getLogger(LogBackController.class);
-    
+	
+	private static final Logger logger = LoggerFactory.getLogger(LogBackController.class);
     @RequestMapping("/test1")
     String test(HttpServletRequest req){
         logger.info("测试日志");
+        logger.error("error");
         return "Hello World!";
     }
 }

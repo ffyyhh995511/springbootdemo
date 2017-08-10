fc22f927d4b1802f9c2f37e9ab36269be9c43c0f
方云鹤 <fangyunhe@mingbikes.com>
2017-08-10 18:31:50 +0800

    TT

--- src/main/java/com/test/demo/controller/LogBackController.java
+++ src/main/java/com/test/demo/controller/LogBackController.java
@@ -20,4 +20,19 @@ public class LogBackController {
     	 logger.error("日志输出测试 error");
         return "Hello World!";
     }
+    
+    @RequestMapping("/test2")
+    String test(){
+    	int i=1/0; 
+    	/*try {
+    		throw new NullPointerException();
+		} catch (Exception e) {
+			logger.debug("空指针报错 Debug");
+	    	 logger.trace("空指针报错 Trace");
+	    	 logger.info("空指针报错 Info");
+	    	 logger.error("空指针报错 error");
+			logger.error("空指针报错",e);
+		}*/
+        return "Hello World!";
+    }
 }

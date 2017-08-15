4660f0eec99bcedcc4ba9a08817062f0524f8122
方云鹤 <842241090@qq.com>
2017-08-15 11:15:44 +0800

    test

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -1,14 +1,24 @@
 package com.test.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 @EnableTransactionManagement // 启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 @SpringBootApplication
 public class DemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+    
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DemoApplication.class);
-//	    app.setBannerMode(Banner.Mode.OFF);
-	    app.run(args);
+		// app.setBannerMode(Banner.Mode.OFF);
+		ApplicationContext ctx = app.run(args);
+		String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+		for (String profile : activeProfiles) {
+			logger.warn("Spring Boot 使用profile为:{}", profile);
+		}
 	}
 }
